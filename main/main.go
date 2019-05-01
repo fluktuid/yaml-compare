d3cf7b00eeb80c56d9a4cb2420d2ad5715b12b84
Paluch, Lukas <lukas.paluch@exxeta.com>
2019-05-01 11:28:05 +0200

    initial code commit

---
+++ main/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+)
+
+type Node struct {
+	value    string
+	parent   *Node
+	children []*Node
+	indent   int
+}
+
+func main() {
+	//	wordPtr := flag.String("word", "foo", "a string")
+	//	numbPtr := flag.Int("numb", 42, "an int")
+	//	boolPtr := flag.Bool("fork", false, "a bool")
+
+	//	var svar string
+	//	flag.StringVar(&svar, "svar", "bar", "a string var")
+	flag.Parse()
+
+	//	fmt.Println("word:", *wordPtr)
+	//	fmt.Println("numb:", *numbPtr)
+	//	fmt.Println("fork:", *boolPtr)
+	//	fmt.Println("svar:", svar)
+
+	arguments := flag.Args()
+
+	fmt.Println(arguments)
+
+	for _, file := range arguments {
+		lines, _ := readFileWithReadLine(file)
+		//		fmt.Println(lines)
+		//		fmt.Println("===")
+		node := toNode(lines)
+		toString(&node)
+	}
+}
+
+func toString(node *Node) {
+	println(node.indent, ":", node.value)
+	for _, c := range node.children {
+		toString(c)
+	}
+}
+
+func toNode(lines []string) Node {
+	root := Node{value: "FILE", parent: nil, indent: -1}
+	lastLineNode := &root
+	for _, line := range lines {
+		var node = Node{value: line, indent: getIndent(line)}
+
+		parent := lastLineNode
+		for parent.indent >= node.indent {
+			parent = (*parent).parent
+		}
+		node.parent = parent
+		parent.children = append(parent.children, &node)
+
+		lastLineNode = &node
+	}
+	return root
+}
+
+func getIndent(line string) int {
+	r, _ := regexp.Compile("^\\s*")
+	return len(r.FindString(line))
+}
+
+func readFileWithReadLine(fn string) ([]string, error) {
+	var rows []string
+	fmt.Println("readFileWithReadLine")
+
+	file, err := os.Open(fn)
+	defer file.Close()
+
+	if err != nil {
+		return rows, err
+	}
+
+	// Start reading from the file with a reader.
+	reader := bufio.NewReader(file)
+
+	for {
+		var buffer bytes.Buffer
+
+		var l []byte
+		var isPrefix bool
+		for {
+			l, isPrefix, err = reader.ReadLine()
+			buffer.Write(l)
+
+			// If we've reached the end of the line, stop reading.
+			if !isPrefix {
+				break
+			}
+
+			// If we're just at the EOF, break
+			if err != nil {
+				break
+			}
+		}
+
+		if err == io.EOF {
+			break
+		}
+
+		line := buffer.String()
+
+		// Process the line here.
+		//		fmt.Println(line)
+		rows = append(rows, line)
+	}
+
+	if err != io.EOF {
+		fmt.Printf(" > Failed!: %v\n", err)
+	}
+
+	return rows, nil
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
