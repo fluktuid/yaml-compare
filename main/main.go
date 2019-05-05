d835fef5fa1bc18ecb4b0883330783bb7de739c9
Paluch, Lukas <lukas.paluch@exxeta.com>
2019-05-05 16:32:17 +0200

    added version removed wrong todo

--- main/main.go
+++ main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	. "github.com/logrusorgru/aurora"
 	flag "github.com/spf13/pflag"
 	"os"
 	"yaml-compare/files"
@@ -10,21 +11,29 @@ import (
 )
 
 func main() {
+	version := flag.BoolP("version", "V", false, "Print Version and exit")
 	resolveAnchors := flag.BoolP("resolve-anchors", "R", true, "Resolve Yaml Anchors, e.g. '&id001'")
 	printLineTypes := flag.BoolP("print-line-types", "L", false, "Print the Line Types, e.g. 'ListItem'")
 	printFiles := flag.BoolP("print", "p", false, "Print files after anchor resolving")
 	printComplete := flag.BoolP("print-complete", "c", false, "Print the complete diff file after comparing")
 	colorLess := flag.BoolP("white", "w", false, "Print without ANSI color")
-	// TODO: implement
 	bewareAnchors := flag.BoolP("beware-anchors", "A", false, "Beware anchor names while resolving (not implemented)")
-	// TODO: implement
 	bewarePointer := flag.BoolP("beware-pointer", "P", false, "Beware pointer names while resolving (not implemented)")
 	fullQualifierName := flag.BoolP("full-qualifier-name", "f", false, "use full-qualifier names, e.g. 'step[0].instrument' (alpha)")
 	flag.Parse()
 
+	if *version {
+		fmt.Println(Underline("Yaml Compare"))
+		fmt.Println("Version: 0.1.0-SNAPSHOT")
+		fmt.Println("Author: Lukas f. Paluch")
+		os.Exit(0)
+		return
+	}
+	flag.Usage()
 	arguments := flag.Args()
 	if len(arguments) < 1 || len(arguments) > 2 {
 		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", errors.New("please specify one or two Files"))
+		flag.Usage()
 		os.Exit(1)
 		return
 	}
